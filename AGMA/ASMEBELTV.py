54914d9ad241f36c764d3a0a06b7c169e1cf8ff6
Carlos Daniel Marceleño Rodriguez <carlosd.marceleno@gmail.com>
2019-12-03 12:08:57 -0600

    Update ASMEBELTV.py

--- AGMA/ASMEBELTV.py
+++ AGMA/ASMEBELTV.py
@@ -8,12 +8,13 @@ p=2.5#Relacion de velocidad
 D2=p*D1
 vb=(D1*w1)/(2)#velocidad de la banda
 #vb=(D2*w2)/(2)#velocidad de la banda
-#en primera instancia se propone una distancia de
-C=D1+D2
+#Se selecciona la banda que se acerque a las especificaciones de potencia a las especificaciones de potencia la cuales dependende de modulo de elasticidad (estudiado por Thomas Young) del material con el que esten fabricadas y el segundo momento de inercia  
+L=75#[inch/mm] Contitech V3/5VX750
 L=2*C+1.57*(D2+D1)+(math.pow((D2-D1),2))/(4*C)
-#Se selecciona la banda que se acercar a las especificaciones de potencia
+#O en primera instancia se propone una distancia de centros
+#C=D1+D2
 B=4*L-6.28*(D2+D1)
-#Se y se ajusta la distancia C entre centros tal que cumpla
+#Se y se re calcula la distancia C entre centros tal que cumpla
 C=(B+math.sqrt(math.pow(B,2)-32*math.pow((D2-D1),2)))/(16)
 L=2*C+1.57*(D2+D1)+(math.pow((D2-D1),2))/(4*C)
 #La longitud S de "span" entre las dos poleas
