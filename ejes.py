9417d1fb96176e288f1c59da6c6b5b1fedd4df09
Carlos Daniel Marceleño Rodriguez <carlosd.marceleno@gmail.com>
2020-02-11 08:23:45 -0600

    Update ejes.py

--- ejes.py
+++ ejes.py
@@ -3,7 +3,7 @@ d=1.618#diametro[mm]
 l=10#de longitud [mm]
 #Teorema de ejes paralelos para cambiar de pocision en el plano xy el eje de giro
 #Teorema de ejes perpendiculares Jo=Ix+Iy para calcular el momento plar de inercia
-J0=(0.5*pi)*(d/2)**4#Momento polar de inercia
+J0=(0.5*pi)*(d/2)**4#Momento polar de inercia ("Torsion")
 Ix=(0.25*pi)*(d/2)**4#Momento de inercia de area o segundo momento de inercia (simetricos Ix=Iy )
 M=F*l#Momento flector
 sig=M*(d/2)/Ix#esfuerzo normales max por flexion
